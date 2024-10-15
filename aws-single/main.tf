e6d30b37651e4187ad721f347e658eb0af4a6b9c
Erik Scholten <tazz74@gmail.com>
2024-10-15 16:29:05 +0200

    Update main.tf

--- aws-single/main.tf
+++ aws-single/main.tf
@@ -14,7 +14,7 @@ provider "aws" {
 
 resource "aws_instance" "Demo_server" {
   ami           = "ami-0084a47cc718c111a"
-  instance_type = "t2.small"
+  instance_type = "t2.micro"
 
   tags = {
     Name = "ExampleAppServerInstance"
